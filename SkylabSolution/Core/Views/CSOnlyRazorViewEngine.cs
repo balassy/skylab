86a5b2d999026f48041d81062bef8336c91bb3f1
gybalassy <balassy@avalon.aut.bme.hu>
2014-03-01 19:45:20 +0100

    Added the C#-only view engine.

---
+++ SkylabSolution/Core/Views/CSOnlyRazorViewEngine.cs
@@ -0,0 +1,55 @@
+﻿namespace Skylab.Core.Views
+{
+	using System.Web.Mvc;
+
+	/// <summary>
+	/// Custom Razor view engine that searches only for .cshtml files and skips the .vbhtml paths.
+	/// </summary>
+	/// <remarks>
+	/// Read more at http://stackoverflow.com/questions/6049445/make-asp-net-mvc-3-razor-view-engine-ignore-vbhtml-files.
+	/// </remarks>
+	public class CSOnlyRazorViewEngine : RazorViewEngine
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CSOnlyRazorViewEngine"/> class.
+		/// </summary>
+		public CSOnlyRazorViewEngine()
+		{
+			this.AreaViewLocationFormats = new[] 
+			{ 
+				"~/Areas/{2}/Views/{1}/{0}.cshtml",
+				"~/Areas/{2}/Views/Shared/{0}.cshtml"
+			};
+
+			this.AreaMasterLocationFormats = new[] 
+			{ 
+				"~/Areas/{2}/Views/{1}/{0}.cshtml",
+				"~/Areas/{2}/Views/Shared/{0}.cshtml"
+			};
+
+			this.AreaPartialViewLocationFormats = new[] 
+			{ 
+				"~/Areas/{2}/Views/{1}/{0}.cshtml",
+				"~/Areas/{2}/Views/Shared/{0}.cshtml"
+			};
+
+			this.ViewLocationFormats = new[] 
+			{
+				"~/Views/{1}/{0}.cshtml",
+				"~/Views/Shared/{0}.cshtml"
+			};
+
+			this.PartialViewLocationFormats = new[] 
+			{
+				"~/Views/{1}/{0}.cshtml",
+				"~/Views/Shared/{0}.cshtml"
+			};
+
+			this.MasterLocationFormats = new[] 
+			{
+				"~/Views/{1}/{0}.cshtml",
+				"~/Views/Shared/{0}.cshtml"
+			};
+		}
+	}
+}
--- SkylabSolution/Web/Global.asax.cs
+++ SkylabSolution/Web/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Skylab.Core.Views;
 
 namespace Skylab.Web
 {
@@ -11,6 +12,10 @@ namespace Skylab.Web
 		[SuppressMessage( "Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Convention-based configuration." )]
 		protected void Application_Start()
 		{
+			// Clear all previously registered view engines and register only the Razor view engine with only .cshtml extensions.
+			ViewEngines.Engines.Clear();
+			ViewEngines.Engines.Add( new CSOnlyRazorViewEngine() );
+
 			AreaRegistration.RegisterAllAreas();
 			FilterConfig.RegisterGlobalFilters( GlobalFilters.Filters );
 			RouteConfig.RegisterRoutes( RouteTable.Routes );
@@ -19,6 +24,7 @@ namespace Skylab.Web
 			// Security: Remove the X-AspNetMVc-Version: 5.0 HTTP header.
 			MvcHandler.DisableMvcResponseHeader = true;
 		}
+
 
 		protected void Application_PreSendRequestHeaders()
 		{
