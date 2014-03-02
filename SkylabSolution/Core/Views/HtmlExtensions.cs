8dc5de1b17dc742839a52667643460cf186a2b70
gybalassy <balassy@avalon.aut.bme.hu>
2014-03-02 05:49:52 +0100

    Obfuscating e-mail addresses.

---
+++ SkylabSolution/Core/Views/HtmlExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skylab.Core.Views
+{
+	public static class HtmlExtensions
+	{
+		/// <summary>
+		/// Renders a hyperlink that links to an e-mail address. The e-mail address is encoded to mislead spam bots.
+		/// </summary>
+		/// <param name="html">The <see cref="HtmlHelper"/> this method extends.</param>
+		/// <param name="text">The visible text of the generated hyperlink. It is also rendered in encoded form.</param>
+		/// <param name="emailAddress">The e-mail address to link to.</param>
+		/// <param name="subject">The proposed subject of the e-mail.</param>
+		/// <param name="body">The proposed body of the e-mail.</param>
+		/// <returns>A raw <c>a</c> tag that renders the link.</returns>
+		[SuppressMessage( "Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "html", Justification = "Extension method to conform with other MVC helpers." )]
+		[SuppressMessage( "Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0", Justification = "Extension method." )]
+		public static IHtmlString EmailLink( this HtmlHelper html, string text, string emailAddress, string subject = null, string body = null )
+		{
+			// Input validation.
+			if( String.IsNullOrEmpty( text ) || String.IsNullOrEmpty( emailAddress ) )
+			{
+				return null;
+			}
+
+			// Encode the sensitive data.
+			string encodedEmailAddress = BitConverter.ToString( Encoding.ASCII.GetBytes( emailAddress ) ).Replace( "-", String.Empty );
+			string encodedText = BitConverter.ToString( Encoding.ASCII.GetBytes( text ) ).Replace( "-", String.Empty );
+
+			TagBuilder tag = new TagBuilder( "a" );
+			tag.AddCssClass( "email-link" );
+			tag.MergeAttribute( "href", "#" );
+			tag.MergeAttributes( new Dictionary<string, string>
+				{
+					{ "href", "#" },
+					{ "data-code", encodedEmailAddress },
+					{ "data-text", encodedText },
+					{ "data-subject", subject },
+					{ "data-body", body }
+				} );
+
+			return new HtmlString( tag.ToString( TagRenderMode.Normal ) );
+		}
+	}
+}
--- SkylabSolution/Web/Static/js/msdnaa.js
+++ SkylabSolution/Web/Static/js/msdnaa.js
@@ -1,39 +1,88 @@
-$( document ).ready( function() {
+$(document).ready(function () {
 
-	$.fn.max = function(selector) { 
-	    return Math.max.apply(null, this.map(function(index, el) { return selector.apply(el); }).get() ); 
-	}
-
+    $.fn.max = function (selector) {
+        return Math.max.apply(null, this.map(function (index, el) { return selector.apply(el); }).get());
+    };
 
     $('.content-box')
     	.height(function () {
-	        var maxHeight = $(this).closest('.content-container').find('.content-box').max( function () {
-	            return $(this).height();
-	        });
-	        return maxHeight;
-	    })
+    	    var maxHeight = $(this).closest('.content-container').find('.content-box').max(function () {
+    	        return $(this).height();
+    	    });
+    	    return maxHeight;
+    	})
 	    .hover(
-	    	function()
-	    	{
-	    		$(this).css( 'background-color', '#f3f3f3' ); 
+	    	function () {
+	    	    $(this).css('background-color', '#f3f3f3');
 	    	},
-	    	function()
-	    	{
-	    		$(this).css( 'background-color', '#eee' );
+	    	function () {
+	    	    $(this).css('background-color', '#eee');
 	    	}
 	    );
-    
-    
-    if( window.location.href.indexOf( 'index.html' ) > 0 || window.location.href === 'http://msdnaa.bme.hu/' )
-    	$( '#mnuKezdolap' ).addClass( 'active' ); 
 
-    if( window.location.href.indexOf( 'letoltes.htm' ) > 0 )
-    	$( '#mnuLetoltes' ).addClass( 'active' ); 
 
-    if( window.location.href.indexOf( 'gyik.htm' ) > 0 )
-    	$( '#mnuGyik' ).addClass( 'active' ); 
+    function decodeEmail(encodedEmail) {
+        /// <summary>
+        /// Decodes the encoded e-mail address given as parameter.
+        /// </summary>
+        /// <param name="encodedEmail" type="String">The encoded e-mail address.</param>
 
-    if( window.location.href.indexOf( 'kapcsolatfelvetel.htm' ) > 0 )
-    	$( '#mnuKapcsolat' ).addClass( 'active' ); 
+        var email = '';
+        for (var i = 0; i < encodedEmail.length;) {
+            var letter = '';
+            letter = encodedEmail.charAt(i) + encodedEmail.charAt(i + 1);
+            email += String.fromCharCode(parseInt(letter, 16));
+            i += 2;
+        }
+        return email;
+    };
+
+
+    function sendEmail(encodedEmail, subject, body) {
+        /// <summary>
+        /// Opens the default e-mail client and initializes the e-mail, subject and body fields.
+        /// </summary>
+        /// <param name="encodedEmail" type="String">The encoded e-mail address.</param>
+        /// <param name="subject" type="String" optional="true">The subject of the e-mail.</param>
+        /// <param name="body" type="String" optional="true">The body text of the e-mail.</param>
+
+        // Decode the encoded e-mail address.
+        var email = decodeEmail(encodedEmail);
+
+        // Build a "mailto" URL.
+        var url = 'mailto:' + email;
+        if (typeof subject != 'undefined' && subject.length > 0) {
+            url += '?Subject=' + subject;
+        }
+        if (typeof body != 'undefined' && body.length > 0) {
+            url += '&Body=' + body;
+        }
+
+        // Redirect the browser to the "mailto" URL.
+        window.location.href = url;
+    };
+
+
+    var $emailLinks = $('.email-link');
+    $emailLinks.each(function () {
+        $(this)
+            .html(decodeEmail($(this).data('text')))
+            .click(function (e) {
+                e.preventDefault();
+                sendEmail($(this).data('code'), $(this).data('subject'), $(this).data('body'));
+            });
+    });
+
+    if (window.location.href.indexOf('index.html') > 0 || window.location.href === 'http://msdnaa.bme.hu/')
+        $('#mnuKezdolap').addClass('active');
+
+    if (window.location.href.indexOf('letoltes.htm') > 0)
+        $('#mnuLetoltes').addClass('active');
+
+    if (window.location.href.indexOf('gyik.htm') > 0)
+        $('#mnuGyik').addClass('active');
+
+    if (window.location.href.indexOf('kapcsolatfelvetel.htm') > 0)
+        $('#mnuKapcsolat').addClass('active');
 
 });
